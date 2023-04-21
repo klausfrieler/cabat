4e070d5777df893aa41127f563b50baa9468c3d2
klaus <kgf@omniversum.de>
2023-04-21 21:23:51 +0200

    Fixed encoding issues by using read_csv. Restored corrupted Latvian dictionary (barred vowels were missing, some deeper issue in R as it seems as low level read and write function replace them with simple vowels.)

--- data-raw/cabat-dict.R
+++ data-raw/cabat-dict.R
@@ -10,27 +10,32 @@ stopifnot(!anyDuplicated(input$key),
           all(input$key == russian$key))
 input$RU <- russian$RU
 
-spanish <- read.csv("data-raw/dict-spanish.csv", stringsAsFactors = FALSE, fileEncoding = "utf-8")
+#spanish <- read.csv("data-raw/dict-spanish.csv", stringsAsFactors = FALSE, fileEncoding = "utf-8")
+spanish <- readr::read_csv("data-raw/dict-spanish.csv")
 names(spanish)[[1]] <- "key"
 stopifnot(all(input$key == spanish$key))
 input$ES <- spanish$ES
 
-dutch <- read.csv("data-raw/dict-dutch.csv", stringsAsFactors = FALSE, fileEncoding = "utf-8")
+#dutch <- read.csv("data-raw/dict-dutch.csv", stringsAsFactors = FALSE, fileEncoding = "utf-8")
+dutch <- readr::read_csv("data-raw/dict-dutch.csv")
 names(dutch)[[1]] <- "key"
 stopifnot(all(input$key == dutch$key))
 input$NL <- dutch$NL
 
-italian <- read.csv("data-raw/dict-italian.csv", stringsAsFactors = FALSE, fileEncoding = "utf-8")
+#italian <- read.csv("data-raw/dict-italian.csv", stringsAsFactors = FALSE, fileEncoding = "utf-8")
+italian <- readr::read_csv("data-raw/dict-italian.csv")
 names(italian)[[1]] <- "key"
 stopifnot(all(input$key == italian$key))
 input$IT <- italian$IT
 
-latvian <- read.csv("data-raw/dict-latvian.csv", stringsAsFactors = FALSE, fileEncoding = "utf-8")
+#latvian <- read.csv("data-raw/dict-latvian.csv", stringsAsFactors = FALSE, fileEncoding = "utf-8")
+latvian <- readr::read_csv("data-raw/dict-latvian.csv")
 names(latvian)[[1]] <- "key"
 stopifnot(all(input$key == latvian$key))
 input$LV <- latvian$lv
 
-de_f <- read.csv("data-raw/dict-german-formal.csv", stringsAsFactors = FALSE, fileEncoding =  "utf-8")
+#de_f <- read.csv("data-raw/dict-german-formal.csv", stringsAsFactors = FALSE, fileEncoding =  "utf-8")
+de_f <- readr::read_csv("data-raw/dict-german-formal.csv")
 names(de_f)[[1]] <- "key"
 stopifnot(all(input$key == de_f$key))
 input$DE_F <- de_f$DE_F
